aa3498be715830558cebff6553d8d795b167fc07
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2015-12-03 15:41:07 -0800

    deleted old file output functions

--- source/first_steps/filehandling.h
+++ source/first_steps/filehandling.h
@@ -8,15 +8,5 @@ void h5_create_empty_by_path(char *name);
 void h5_write_buffers_to_disk(hsize_t Nt);
 void h5_close();
 void save();
-void file_single_write_by_name_1d(double *data, size_t N, size_t row_skip,
-                            char *name);
-void file_create_empty_by_path(char *filepath);
-void file_create_empty_by_name(char *name);
-void file_append_by_name_1d(double *data, size_t N, size_t row_skip,
-                                char *name);
-void file_append_by_path_1d(double *v, size_t N, size_t row_skip,
-							char *filename);
-void file_append_2d(double *A, size_t N, size_t M, size_t row_skip,
-                            size_t col_skip, char *filename);
 
 #endif
