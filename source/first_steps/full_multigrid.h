b354f0f014f9ab0c1a74517b77fde06e3fb83e93
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-02-15 15:29:26 -0800

    readded full multigrid files, might still work

---
+++ source/first_steps/full_multigrid.h
@@ -0,0 +1,19 @@
+#ifndef __FULL_MULTIGRID__
+#define __FULL_MULTIGRID__
+
+#include <stddef.h>
+
+#define NPRE            (1)
+#define NPOST           (1)
+#define NGMAX           (15)
+
+void mglin(double **u, size_t n, size_t ncycle);
+void addint(double **uf, double **uc, double **res, size_t nf);
+void copy(double **aout, double **ain, size_t n);
+void fill0(double **u, size_t n);
+void interp(double **uf, double **uc, size_t nf);
+void relax(double **u, double **rhs, size_t n);
+void resid(double **res, double **u, double **rhs, size_t n);
+void rstrct(double **uc, double **uf, size_t nc);
+void slvsml(double **u, double **rhs);
+#endif
