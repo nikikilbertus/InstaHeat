253738d86c448b9f8d968e65635b060bf52751c6
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-02-29 14:59:57 -0800

    removed fileid arugment from write parameter function, because accessible in pars.file.id

--- source/first_steps/filehandling.h
+++ source/first_steps/filehandling.h
@@ -7,8 +7,7 @@ void h5_create_empty_by_path(const char *name);
 void h5_create_dset(const hsize_t rank, const hsize_t *dim,
         const hsize_t *max_dim, const hsize_t *chunk, hsize_t *dset_id,
         const char *name);
-void h5_write_parameter(const hid_t file, const char *name, const double *val,
-        size_t N);
+void h5_write_parameter(const char *name, const double *val, size_t N);
 void h5_write_all_buffers(const hsize_t Nt);
 void h5_get_extent(hsize_t *max, hsize_t *cur);
 void h5_close();
