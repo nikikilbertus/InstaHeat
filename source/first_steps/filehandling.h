5dc5e7249cca0a5d7637ab3ef426b4968603be86
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-03-01 14:13:01 -0800

    added defines for dataset names to filehandling.h

--- source/first_steps/filehandling.h
+++ source/first_steps/filehandling.h
@@ -3,6 +3,35 @@
 
 #include "hdf5.h"
 
+#define     H5_PHI_NAME                 ("phi")
+#define     H5_DPHI_NAME                ("dphi")
+#define     H5_PSI_NAME                 ("psi")
+#define     H5_DPSI_NAME                ("dpsi")
+#define     H5_RHO_NAME                 ("rho")
+#define     H5_POWER_SPECTRUM_NAME      ("power_spectrum")
+#define     H5_TIME_NAME                ("time")
+#define     H5_A_NAME                   ("a")
+#define     H5_PHI_MEAN_NAME            ("phi_mean")
+#define     H5_PHI_VARIANCE_NAME        ("phi_variance")
+#define     H5_DPHI_MEAN_NAME           ("dphi_mean")
+#define     H5_DPHI_VARIANCE_NAME       ("dphi_variance")
+#define     H5_PSI_MEAN_NAME            ("psi_mean")
+#define     H5_PSI_VARIANCE_NAME        ("psi_variance")
+#define     H5_DPSI_MEAN_NAME           ("dpsi_mean")
+#define     H5_DPSI_VARIANCE_NAME       ("dpsi_variance")
+#define     H5_RHO_MEAN_NAME            ("rho_mean")
+#define     H5_RHO_VARIANCE_NAME        ("rho_variance")
+#define     H5_MASS_NAME                ("mass")
+#define     H5_DIMENSION_NAME           ("dimension")
+#define     H5_SEED_NAME                ("seed")
+#define     H5_STRIDES_TIME_NAME        ("strides_time")
+#define     H5_STRIDES_SPACE_NAME       ("strides_space")
+#define     H5_TOLERANCES_NAME          ("tolerances")
+#define     H5_GRIDPOINTS_INTERNAL_NAME ("gridpoints_internal")
+#define     H5_GRIDPOINTS_OUTPUT_NAME   ("gridpoints_output")
+#define     H5_COMMIT_HASH_NAME         ("commit_hash")
+
+
 void h5_create_empty_by_path(const char *name);
 void h5_create_dset(const hsize_t rank, const hsize_t *dim,
         const hsize_t *max_dim, const hsize_t *chunk, hsize_t *dset_id,
