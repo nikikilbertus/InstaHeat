e1bafef7509807d9fca54a35df9d23933f6ec45a
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-03-10 21:13:19 -0800

    cosmetics

--- source/first_steps/main.c
+++ source/first_steps/main.c
@@ -5,8 +5,10 @@
 #include <fftw3.h>
 #include <time.h>
 #include <sys/time.h>
-/* #include <gperftools/profiler.h> */
 #include "main.h"
+#ifdef ENABLE_PROFILER
+#include <gperftools/profiler.h>
+#endif
 #include "setup.h"
 #include "RK4_stepper.h"
 #include "dopri853_stepper.h"
