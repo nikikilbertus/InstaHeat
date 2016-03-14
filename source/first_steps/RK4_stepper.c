72c947c464a5e5f5f89467a3b29be150f34d87ef
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-03-13 20:26:50 -0700

    changed else on same line...

--- source/first_steps/RK4_stepper.c
+++ source/first_steps/RK4_stepper.c
@@ -57,8 +57,7 @@ void run_rk4()
             evo_flags.compute_pow_spec = 0;
             mk_means_and_variances();
             save();
-        }
-        else {
+        } else {
             mk_rhs(t, field, k1);
         }
 
