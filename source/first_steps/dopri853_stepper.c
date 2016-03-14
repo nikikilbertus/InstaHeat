4a7fe5354012f081dc825dcf71fb9db5c84efa6d
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-03-13 20:17:51 -0700

    changed starting braces of if/for/else...

--- source/first_steps/dopri853_stepper.c
+++ source/first_steps/dopri853_stepper.c
@@ -98,40 +98,32 @@ void run_dopri853()
     double secs = -get_wall_time();
     #endif
 
-    for (dp.n_stp = 0; dp.n_stp < dp.max_steps; ++dp.n_stp)
-    {
-        if (dp.t + dp.dt * 1.0001 > dp.tf)
-        {
+    for (dp.n_stp = 0; dp.n_stp < dp.max_steps; ++dp.n_stp) {
+        if (dp.t + dp.dt * 1.0001 > dp.tf) {
             dp.dt = dp.tf - dp.t;
             INFO(printf("overshoot, new dt = %f\n", dp.dt));
         }
-        if (perform_step(dp.dt))
-        {
+        if (perform_step(dp.dt)) {
             break;
         }
-        if (dp.dt_did == dp.dt)
-        {
+        if (dp.dt_did == dp.dt) {
             ++dp.n_ok;
         }
-        else
-        {
+        else {
             ++dp.n_bad;
         }
         #ifdef DEBUG
         INFO(printf("did step: %d with dt: %f\n", dp.n_stp, dp.dt_did));
         #endif
 
-        if ((dp.n_stp + 1) % pars.file.skip == 0)
-        {
+        if ((dp.n_stp + 1) % pars.file.skip == 0) {
             mk_means_and_variances();
             save();
         }
-        if (dp.t >= dp.tf)
-        {
+        if (dp.t >= dp.tf) {
             break;
         }
-        if (fabs(dp.dt_next) <= dp.dt_min)
-        {
+        if (fabs(dp.dt_next) <= dp.dt_min) {
             fputs("!!! Stepsize underflow.\n", stderr);
             break;
         }
@@ -139,8 +131,7 @@ void run_dopri853()
     }
 
     size_t index = pars.file.index;
-    if (index != 0 && time_buf[index - 1] < dp.t)
-    {
+    if (index != 0 && time_buf[index - 1] < dp.t) {
         prepare_and_save_timeslice();
     }
 
@@ -192,16 +183,13 @@ int perform_step(const double dt_try)
 {
     const size_t Ntot = pars.Ntot;
     double dt = dt_try;
-    for ( ; ; )
-    {
+    for ( ; ; ) {
         try_step(dt);
         double err = error(dt);
-        if (success(err, &dt))
-        {
+        if (success(err, &dt)) {
             break;
         }
-        if (fabs(dt) <= fabs(dp.t) * dp.eps)
-        {
+        if (fabs(dt) <= fabs(dp.t) * dp.eps) {
             fputs("\n!!! Stepsize underflow.\n", stderr);
             return 1;
         }
@@ -209,16 +197,14 @@ int perform_step(const double dt_try)
     #ifdef ENABLE_FFT_FILTER
     apply_filter_real(field_new);
     #endif
-    if ((dp.n_stp + 1) % pars.file.skip == 0)
-    {
+    if ((dp.n_stp + 1) % pars.file.skip == 0) {
         evo_flags.compute_pow_spec = 1;
     }
     mk_rhs(dp.t + dt, field_new, dfield_new);
     evo_flags.compute_pow_spec = 0;
 
     #pragma omp parallel for
-    for (size_t i = 0; i < Ntot; ++i)
-    {
+    for (size_t i = 0; i < Ntot; ++i) {
         field[i] = field_new[i];
         dfield[i] = dfield_new[i];
     }
@@ -256,16 +242,14 @@ void try_step(const double dt)
 
     // ------------ 2 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt * dpc.a21 * dfield[i];
     }
     mk_rhs(t + dpc.c2 * dt, dpv.k_tmp, dpv.k2);
 
     // ------------ 3 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a31 * dfield[i] + dpc.a32 * dpv.k2[i]);
     }
@@ -273,8 +257,7 @@ void try_step(const double dt)
 
     // ------------ 4 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a41 * dfield[i] + dpc.a43 * dpv.k3[i]);
     }
@@ -282,8 +265,7 @@ void try_step(const double dt)
 
     // ------------ 5 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a51 * dfield[i] + dpc.a53 * dpv.k3[i] + dpc.a54 * dpv.k4[i]);
     }
@@ -291,8 +273,7 @@ void try_step(const double dt)
 
     // ------------ 6 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a61 * dfield[i] + dpc.a64 * dpv.k4[i] + dpc.a65 * dpv.k5[i]);
     }
@@ -300,8 +281,7 @@ void try_step(const double dt)
 
     // ------------ 7 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a71 * dfield[i] + dpc.a74 * dpv.k4[i] + dpc.a75 * dpv.k5[i] +
              dpc.a76 * dpv.k6[i]);
@@ -310,8 +290,7 @@ void try_step(const double dt)
 
     // ------------ 8 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a81 * dfield[i] + dpc.a84 * dpv.k4[i] + dpc.a85 * dpv.k5[i] +
              dpc.a86 * dpv.k6[i] + dpc.a87 * dpv.k7[i]);
@@ -320,8 +299,7 @@ void try_step(const double dt)
 
     // ------------ 9 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a91 * dfield[i] + dpc.a94 * dpv.k4[i] + dpc.a95 * dpv.k5[i] +
              dpc.a96 * dpv.k6[i] + dpc.a97 * dpv.k7[i] + dpc.a98 * dpv.k8[i]);
@@ -330,8 +308,7 @@ void try_step(const double dt)
 
     // ------------ 10 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a101 * dfield[i] + dpc.a104 * dpv.k4[i] + dpc.a105 * dpv.k5[i] +
              dpc.a106 * dpv.k6[i] + dpc.a107 * dpv.k7[i] + dpc.a108 * dpv.k8[i] +
@@ -341,8 +318,7 @@ void try_step(const double dt)
 
     // ------------ 11 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a111 * dfield[i] + dpc.a114 * dpv.k4[i] + dpc.a115 * dpv.k5[i] +
              dpc.a116 * dpv.k6[i] + dpc.a117 * dpv.k7[i] + dpc.a118 * dpv.k8[i] +
@@ -355,8 +331,7 @@ void try_step(const double dt)
 
     // ------------ 12 ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k_tmp[i] = field[i] + dt *
             (dpc.a121 * dfield[i] + dpc.a124 * dpv.k4[i] + dpc.a125 * dpv.k5[i] +
              dpc.a126 * dpv.k6[i] + dpc.a127 * dpv.k7[i] + dpc.a128 * dpv.k8[i] +
@@ -366,8 +341,7 @@ void try_step(const double dt)
 
     // ------------ step ahead ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.k4[i] = dpc.b1 * dfield[i] + dpc.b6 * dpv.k6[i] + dpc.b7 * dpv.k7[i] +
             dpc.b8 * dpv.k8[i] + dpc.b9 * dpv.k9[i] + dpc.b10 * dpv.k10[i] +
             dpc.b11 * dpv.k2[i] + dpc.b12 * dpv.k3[i];
@@ -377,8 +351,7 @@ void try_step(const double dt)
 
     // ------------ error estimates ------------
     #pragma omp parallel for
-    for (i = 0; i < Ntot; ++i)
-    {
+    for (i = 0; i < Ntot; ++i) {
         dpv.yerr[i]  = dpv.k4[i] - dpc.bhh1 * dfield[i] - dpc.bhh2 * dpv.k9[i] -
                         dpc.bhh3 * dpv.k3[i];
         dpv.yerr2[i] = dpc.er1 * dfield[i] + dpc.er6 * dpv.k6[i] +
@@ -404,8 +377,7 @@ double error(const double dt)
 
     double tmp;
     #pragma omp parallel for private(sk, tmp) reduction(+:err,err2)
-    for (size_t i = 0; i < Ntot; ++i)
-    {
+    for (size_t i = 0; i < Ntot; ++i) {
         sk = dp.a_tol + dp.r_tol * MAX(fabs(field[i]), fabs(field_new[i]));
         tmp = dpv.yerr[i] / sk;
         err2 += tmp * tmp;
@@ -413,8 +385,7 @@ double error(const double dt)
         err += tmp * tmp;
     }
     deno = err + 0.01 * err2;
-    if (deno <= 0.0)
-    {
+    if (deno <= 0.0) {
         deno = 1.0;
     }
     return dt * err * sqrt(1.0 / (Ntot * deno));
@@ -446,36 +417,28 @@ int success(const double err, double *dt)
     const double maxscale = dp.maxscale;
     double scale;
 
-    if (err <= 1.0)
-    {
-        if (err == 0.0)
-        {
+    if (err <= 1.0) {
+        if (err == 0.0) {
             scale = maxscale;
         }
-        else
-        {
+        else {
             scale = safe * pow(err, -alpha) * pow(dp.err_old, beta);
-            if (scale < minscale)
-            {
+            if (scale < minscale) {
                 scale = minscale;
             }
-            if (scale > maxscale)
-            {
+            if (scale > maxscale) {
                 scale = maxscale;
             }
         }
-        if (dp.reject)
-        {
+        if (dp.reject) {
             dp.dt_next = (*dt) * MIN(scale, 1.0);
         }
-        else
-        {
+        else {
             dp.dt_next = (*dt) * scale;
         }
         #ifdef MAX_DT_HUBBLE_FRACTION
         const double minstep = MAX_DT_HUBBLE_FRACTION * sqrt(3.0 / rho_mean);
-        if (dp.dt_next > minstep)
-        {
+        if (dp.dt_next > minstep) {
             dp.dt_next = minstep;
         }
         #endif
@@ -484,8 +447,7 @@ int success(const double err, double *dt)
         dp.reject = 0;
         return 1;
     }
-    else
-    {
+    else {
         scale = MAX(safe * pow(err, -alpha), minscale);
         (*dt) *= scale;
         dp.reject = 1;
@@ -519,8 +481,7 @@ void allocate_dopri853_values()
     dpv.yerr2 = fftw_malloc(Ntot * sizeof *dpv.yerr2);
 
     if (!(dpv.k2 && dpv.k3 && dpv.k4 && dpv.k5 && dpv.k6 && dpv.k7 && dpv.k8 &&
-          dpv.k9 && dpv.k10 && dpv.k_tmp && dpv.yerr && dpv.yerr2))
-    {
+          dpv.k9 && dpv.k10 && dpv.k_tmp && dpv.yerr && dpv.yerr2)) {
         fputs("Allocating memory failed.\n", stderr);
         exit(EXIT_FAILURE);
     }
