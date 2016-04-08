07cf622d0eee1be55a5135de65f059286a0cc22f
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-08 11:07:54 -0700

    added doxygen doc for setup.h

--- source/first_steps/setup.h
+++ source/first_steps/setup.h
@@ -4,8 +4,15 @@
 #include <stddef.h>
 #include "main.h"
 
-// everything here is only called once in the beginning or the end, thus there
-// is no need for any optimization
+/**
+ * @file setup.h
+ * @brief Function declarations for setup.c
+ * @note The only two functions called from the outside of this file are
+ * allocate_and_initialize_all() and free_and_destroy_all() before and after the
+ * simulation respectively. Moreover they are only called once. Setup and
+ * cleanup is usually fast, so we do not care about performance here.
+ */
+
 void allocate_and_initialize_all();
 void initialize_threading();
 void initialize_parameters();
