1247c773315856cc2a361f8a1258b7a70fc78370
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-14 15:51:30 -0700

    added compute_cstr flag

--- source/simulation/evolution_toolkit.h
+++ source/simulation/evolution_toolkit.h
@@ -17,7 +17,17 @@
 struct evolution_flags
 {
     uint8_t filter; ///< If set, apply filtering (unused)
-    uint8_t compute_pow_spec; ///< If set, compute power spectra
+    /**
+     * @brief If set, compute power spectra via `mk_power_spectrum(const
+     * fftw_complex *in, struct output out)` when calling
+     * `mk_gradient_squared_and_laplacian(double *in)`
+     */
+    uint8_t compute_pow_spec;
+    /**
+     * @brief If set, compute the constraints via `mk_constraints()` when
+     * calling `mk_rhs(const double t, double *f, double *result)`
+     */
+    uint8_t compute_cstr;
 };
 
 extern struct evolution_flags evo_flags; ///< Instance of `evolution_flags`
