858793afe4d13e206d59a9436e82721f7b34d790
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-15 23:30:18 -0700

    minor fixes

--- source/simulation/rkf45.c
+++ source/simulation/rkf45.c
@@ -5,27 +5,25 @@
 #include <fftw3.h>
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_odeiv2.h>
-#include "RK4_stepper.h"
+#include "rkf45.h"
 #include "main.h"
 #include "evolution_toolkit.h"
-#include "filehandling.h"
 
-void runrkf45()
+void run_rkf45()
 {
-    double mu = 10;
     gsl_odeiv2_system sys = {mk_rhs_wrapper, NULL, pars.Nall, NULL};
-    gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45,
-                                    ABSOLUTE_TOLERANCE, RELATIVE_TOLERANCE, 0.0);
-    for (i = 1; i <= 100; i++) {
+    gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new(&sys,
+            gsl_odeiv2_step_rkf45, pars.t.dt, ABSOLUTE_TOLERANCE, RELATIVE_TOLERANCE);
+    for (size_t i = 1; i <= 100; i++) {
         double ti = i * pars.t.tf / 100.0;
         int status = gsl_odeiv2_driver_apply(d, &pars.t.t, ti, field);
         if (status != GSL_SUCCESS) {
           printf ("error, return value=%d\n", status);
           break;
         }
+        printf("t = %f\n", ti);
     }
     gsl_odeiv2_driver_free (d);
-    return 0;
 }
 
 int mk_rhs_wrapper(double t, const double f[], double result[], void * params)
