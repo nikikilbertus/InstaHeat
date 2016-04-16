b43db51f15c84f7aaa87125b62b1e11313b92ac8
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-16 13:08:53 -0700

    renamed RK4_stepper to rk4

--- source/simulation/rk4.h
+++ source/simulation/rk4.h
@@ -4,8 +4,8 @@
 #include "main.h"
 
 /**
- * @file RK4_stepper.h
- * @brief Function declarations for RK4_stepper.c
+ * @file rk4.h
+ * @brief Function declarations for rk4.c
  */
 
 void run_rk4();
