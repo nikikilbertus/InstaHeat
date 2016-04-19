a967f781296039f137201c1ae7f6b54b86b5973e
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-18 21:22:29 -0700

    adjusted to new monitor struct

--- source/simulation/main.c
+++ source/simulation/main.c
@@ -49,11 +49,7 @@ struct k_grid kvec;
 struct temporary tmp;
 fftw_plan p_fw;
 fftw_plan p_bw;
-double fftw_time_exe = 0.0;
-double fftw_time_plan = 0.0;
-double filter_time = 0.0;
-double poisson_time = 0.0;
-double h5_time_write = 0.0;
+struct monitor mon;
 
 /**
  * @brief Main routine: Calls setup, integration and cleanup routines.
@@ -91,23 +87,23 @@ int main(int argc, const char * argv[])
     INFO(printf("main took %f seconds.\n", secs));
     INFO(puts("as percentage of total, not mutually disjoint:"));
     INFO(printf("fftw execution took %f seconds (%.2f %%).\n",
-                        fftw_time_exe, 100. * (fftw_time_exe / secs)));
+                    mon.fftw_time_exe, 100. * (mon.fftw_time_exe / secs)));
     INFO(printf("fftw planning took %f seconds (%.2f %%).\n",
-                        fftw_time_plan, 100. * (fftw_time_plan / secs)));
+                    mon.fftw_time_plan, 100. * (mon.fftw_time_plan / secs)));
     INFO(printf("fft filtering took %f seconds (%.2f %%).\n",
-                        filter_time, 100. * (filter_time / secs)));
+                    mon.filter_time, 100. * (mon.filter_time / secs)));
     INFO(printf("poisson equation took %f seconds (%.2f %%).\n",
-                        poisson_time, 100. * (poisson_time / secs)));
+                    mon.poisson_time, 100. * (mon.poisson_time / secs)));
     INFO(printf("h5 write to disk took %f seconds (%.2f %%).\n",
-                        h5_time_write, 100. * (h5_time_write / secs)));
+                    mon.h5_time_write, 100. * (mon.h5_time_write / secs)));
 
     INFO(puts("Writing runtimes to disk\n"));
     h5_write_parameter(H5_RUNTIME_TOTAL_NAME, &secs, 1);
-    h5_write_parameter(H5_RUNTIME_FFTW_NAME, &fftw_time_exe, 1);
-    h5_write_parameter(H5_RUNTIME_FFTWPLAN_NAME, &fftw_time_plan, 1);
-    h5_write_parameter(H5_RUNTIME_FILTER_NAME, &filter_time, 1);
-    h5_write_parameter(H5_RUNTIME_ELLIPTIC_NAME, &poisson_time, 1);
-    h5_write_parameter(H5_RUNTIME_WRITEOUT_NAME, &h5_time_write, 1);
+    h5_write_parameter(H5_RUNTIME_FFTW_NAME, &mon.fftw_time_exe, 1);
+    h5_write_parameter(H5_RUNTIME_FFTWPLAN_NAME, &mon.fftw_time_plan, 1);
+    h5_write_parameter(H5_RUNTIME_FILTER_NAME, &mon.filter_time, 1);
+    h5_write_parameter(H5_RUNTIME_ELLIPTIC_NAME, &mon.poisson_time, 1);
+    h5_write_parameter(H5_RUNTIME_WRITEOUT_NAME, &mon.h5_time_write, 1);
     #endif
 
     free_and_destroy_all();
