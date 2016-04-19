4725adfc7b1a8f84bca9b941d848b5f71ad04261
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-19 15:12:57 -0700

    made functions local

--- source/simulation/tests.h
+++ source/simulation/tests.h
@@ -4,19 +4,5 @@
 #include "main.h"
 
 void run_all_tests();
-void test_mk_gradient_squared_and_laplacian();
-double test_func_gradsq(const double x, const double y, const double z);
-double test_func_lap(const double x, const double y, const double z);
-double test_func(const double x, const double y, const double z);
-double test_func_Dx(const double x, const double y, const double z);
-double test_func_Dy(const double x, const double y, const double z);
-double test_func_Dz(const double x, const double y, const double z);
-double test_func_D2x(const double x, const double y, const double z);
-double test_func_D2y(const double x, const double y, const double z);
-double test_func_D2z(const double x, const double y, const double z);
-void fill_field(double *f, double (*func)(const double, const double,
-            const double));
-int are_fields_equal(const double *f, const double *g);
-int equal(const double a, const double b);
 
 #endif
