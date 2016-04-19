9b9975095f32e48b678241c0c552f4dd94bb9a2f
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-19 15:11:14 -0700

    made functions local

--- source/simulation/setup.h
+++ source/simulation/setup.h
@@ -14,30 +14,6 @@
  */
 
 void allocate_and_initialize_all();
-void initialize_threading();
-void initialize_parameters();
-void allocate_external();
-void mk_fftw_plans();
-void mk_k_grid();
-void mk_filter_mask();
-extern double filter_window(const double x);
-void mk_initial_conditions();
-void initialize_from_dat();
-void initialize_from_bunch_davies();
-void mk_bunch_davies(double *f, const double H, const double homo,
-        const double gamma);
-extern complex box_muller();
-void initialize_from_internal_function();
-void mk_x_grid(double *grid);
-double phi_init(const double x, const double y, const double z,
-        const double *ph);
-double dphi_init(const double x, const double y, const double z,
-        const double *ph);
-double wrapped_gaussian(const double x, const double y, const double z);
-void mk_initial_psi();
 void free_and_destroy_all();
-void destroy_and_cleanup_fftw();
-void free_external();
-void print_vector(const double *vector, const size_t N);
 
 #endif
