696a1bf07d270435926bf25f4ed81c2f03731ca6
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-04-23 22:20:15 -0700

    cosmetics

--- src/setup.h
+++ src/setup.h
@@ -9,7 +9,7 @@
  * @brief Function declarations for `setup.c`
  *
  * @note Both of the two publicly visible function
- * allocate_and_initialize_all() and free_and_destroy_all() are each called
+ * `allocate_and_initialize_all()` and `free_and_destroy_all()` are each called
  * exactly once before and after the simulation respectively. Setup and cleanup
  * is usually fast compared to the actual integration, so we do not have to
  * worry about performance here.
