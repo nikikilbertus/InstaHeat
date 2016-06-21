e34fabc769745326477175e9869fdb53584c10f3
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-20 19:24:29 -0700

    readded, cleaned up

---
+++ evaluation/mkHamiltonianConstraint.m
@@ -0,0 +1,16 @@
+function [check, t1, t2, t3] = mkHamiltonianConstraint(psi, dpsi, a, rho, L)
+    if ndims(phi) ~= 3
+       error('''mkHamiltonianConstraint'' only works in three dimensions'); 
+    end
+    N = size(psi,1);
+    k = [0:N/2 -N/2+1:-1] * 2*pi/L;
+    [kx,ky,kz] = ndgrid(k,k,k);
+    kk = kx.^2 + ky.^2 + kz.^2;
+    H = sqrt(mean(rho(:)) / 3);
+    psilap = ifftn(-kk.*fftn(psi));
+    % psilap = del2(psi, dx) * 2 * dim;
+    t1 = psilap;
+    t2 = - 3 * H * (H * psi + dpsi) .* a^2;
+    t3 = - 0.5 * (rho - mean(rho(:))) .* a^2;
+    check = t1 + t2 + t3;
+end
