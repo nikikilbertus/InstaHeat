68e2b9590d0fad9969d90b46fcab38b1fcc549fd
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-20 19:20:47 -0700

    cleaned up

--- evaluation/mk3dvideo.m
+++ evaluation/mk3dvideo.m
@@ -1,18 +1,18 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% volume visualization video
+%% volume visualization video
+loadDsets;
+dsets = {'a', 'rho', 'N'};
+readDsets;
+field = rho;
+fieldname = 'rho'; filename = 'rho3d';
 Nt = length(a);
-rho = h5read(name, '/rho');
-rho = reshape(rho, N(1), N(2), N(3), Nt);
+field = reshape(field, N(1), N(2), N(3), Nt);
 
-x = linspace(0,1,N(1)+1)';
-x = x(1:end-1);
-y = linspace(0,1,N(2)+1)';
-y = y(1:end-1);
-z = linspace(0,1,N(3)+1)';
-z = z(1:end-1);
+x = linspace(0,1,N(1)+1)'; x = x(1:end-1);
+y = linspace(0,1,N(2)+1)'; y = y(1:end-1);
+z = linspace(0,1,N(3)+1)'; z = z(1:end-1);
 [X,Y,Z] = meshgrid(x,y,z);
 
-pl = rho(:,:,:,end);
+pl = field(:,:,:,end);
 [~,pos] = max(pl(:));
 [m1, m2, m3] = ind2sub(size(pl),pos);
 pad = 8;
@@ -21,12 +21,12 @@ yy = y(m2-pad:m2+pad);
 zz = z(m3-pad:m3+pad);
 [XX,YY,ZZ] = meshgrid(xx,yy,zz);
 
-writerObj = VideoWriter('rho3d.avi');
+writerObj = VideoWriter([filename '.avi']);
 open(writerObj);
 set(gcf,'Renderer','zbuffer');
 % Create a sequence of frames and write each frame to the file
 for i = 1:length(a)
-pl = rho(:,:,:,i);
+pl = field(:,:,:,i);
 subplot(2,3,1)
 h = slice(X,Y,Z, pl, x, y, z, 'cubic');
 set(h, 'EdgeColor','none', 'FaceColor','interp')
@@ -46,13 +46,16 @@ g = slice(XX,YY,ZZ,pl2, x(m1), y(m2), z(m3), 'cubic');
 set(g, 'EdgeColor','none', 'FaceColor','interp')
 alpha(0.5)
 subplot(2,3,4)
-surf(y,z,log10(squeeze(pl(m1,:,:)))); xlabel('y'); ylabel('z'); zlabel('log10(rho)');
+surf(y,z,log10(squeeze(pl(m1,:,:)))); xlabel('y'); ylabel('z');
+zlabel(['log10(' fieldname ')']);
 shading interp
 subplot(2,3,5)
-surf(x,z,log10(squeeze(pl(:,m2,:)))); xlabel('x'); ylabel('z'); zlabel('log10(rho)');
+surf(x,z,log10(squeeze(pl(:,m2,:)))); xlabel('x'); ylabel('z');
+zlabel(['log10(' fieldname ')']);
 shading interp
 subplot(2,3,6)
-surf(x,y,log10(squeeze(pl(:,:,m3)))); xlabel('x'); ylabel('y'); zlabel('log10(rho)');
+surf(x,y,log10(squeeze(pl(:,:,m3)))); xlabel('x'); ylabel('y');
+zlabel(['log10(' fieldname ')']);
 shading interp
 
 frame = getframe(gcf);
@@ -60,17 +63,18 @@ writeVideo(writerObj,frame);
 end
 close(writerObj);
 
-system('ffmpeg -i rho3d.avi -vcodec libx264 -filter:v "setpts=15.0*PTS" rho3d2.mp4')
+system(['ffmpeg -i rho3d.avi -vcodec libx264 -filter:v "setpts=15.0*PTS"'...
+    filename '.mp4'])
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% initial conditions from karsten
+%% initial conditions from karsten
 
 name = '~/Dropbox/Uni/Exercises/11Semester/MAPhysics/data/karsten/data_64.dat';
 data = importdata(name);
 data = squeeze(data(:,4:5));
 phi = reshape(data(:,1),64,64,64);
 dphi = reshape(data(:,2),64,64,64);
-rho = dphi.^2 / 2 + mass^2 * phi.^2 / 2;
+field = dphi.^2 / 2 + mass^2 * phi.^2 / 2;
 
 writerObj = VideoWriter('ic3d.avi');
 open(writerObj);
@@ -97,13 +101,13 @@ surf(squeeze(dphi(:,:,i)))
 title('dphi projected to xy')
 
 subplot(3,3,7)
-surf(squeeze(rho(i,:,:)))
+surf(squeeze(field(i,:,:)))
 title('rho projected to yz')
 subplot(3,3,8)
-surf(squeeze(rho(:,i,:)))
+surf(squeeze(field(:,i,:)))
 title('rho projected to xz')
 subplot(3,3,9)
-surf(squeeze(rho(:,:,i)))
+surf(squeeze(field(:,:,i)))
 title('rho projected to xy')
 frame = getframe(gcf);
 writeVideo(writerObj,frame);
