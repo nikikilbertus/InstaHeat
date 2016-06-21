8e1e47200f94724ddfbc393ddf5c82dc9f7f385e
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-20 22:31:37 -0700

    check whether name has been provided, always include a and t

--- evaluation/readDsets.m
+++ evaluation/readDsets.m
@@ -1,13 +1,17 @@
-name = 'cutoff/96_16_5e-3_fil';
-name = ['~/Dropbox/Uni/Exercises/12Semester/MAPhysics/data/' name '.h5'];
+if ~exist('fullpath','var')
+    if ~exist('name','var')
+        name = 'cutoff/96_16_5e-3';
+    end
+    fullname = ['~/Dropbox/Uni/Exercises/12Semester/MAPhysics/data/' name '.h5'];
+end
 loadDsets;
 if ~exist('dsets', 'var')
     dsets = dsetsAll;
 end
-
+require('a', 't', dsets);
 % TODO decide whether I want certain indices for the datasets
 % create dsetsIndexed in loadDsets to indicate the ones I need to index
 
 for i = 1:length(dsets)
-    readDset(dictNames, name, dsets{i});
+    readDset(dictNames, fullname, dsets{i});
 end
