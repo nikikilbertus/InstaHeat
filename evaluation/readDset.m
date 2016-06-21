6d422ce139c5e4e929065caa65c98a5088436d12
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-20 22:32:15 -0700

    make everything col major, get individual summary entries, H and rhorms

--- evaluation/readDset.m
+++ evaluation/readDset.m
@@ -1,6 +1,9 @@
 function data = readDset(dictNames, file, dset, I)
     try
         data = h5read(file, ['/' dset]);
+        if size(data,2) > 1
+            data = data';
+        end
     catch
         display(['could not read ' dset])
         return;
@@ -13,5 +16,20 @@ function data = readDset(dictNames, file, dset, I)
             data = data(I);
         end
     end
-    assignin('caller', dictNames(dset), data);
+    if ~isempty(strfind(dset, 'summary'))
+        mkSummary(dictNames, dset, data);
+    else
+        assignin('caller', dictNames(dset), data);
+    end 
+end
+
+function [] = mkSummary(dictNames, dset, data)
+    assignin('base', strrep(dictNames(dset),'S','mean'), data(:,1));
+    assignin('base', strrep(dictNames(dset),'S','std'), sqrt(data(:,2)));
+    assignin('base', strrep(dictNames(dset),'S','min'), data(:,3));
+    assignin('base', strrep(dictNames(dset),'S','max'), data(:,4));
+    if ~isempty(strfind(dset, 'rho'))
+        assignin('base', 'H', sqrt(data(:,1) / 3));
+        assignin('base', 'rhorms', sqrt(data(:,2)) ./ data(:,1));
+    end
 end
