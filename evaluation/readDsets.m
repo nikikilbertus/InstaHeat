ace1b65810bce680b8936d12e2037fef6ea88b85
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-21 20:36:13 -0700

    changed name of loopvar to avoid conflicts

--- evaluation/readDsets.m
+++ evaluation/readDsets.m
@@ -1,7 +1,8 @@
 if ~exist('name','var')
     name = 'cutoff/96_16_5e-3';
 end
-fullname = ['~/Dropbox/Uni/Exercises/12Semester/MAPhysics/data/' name '.h5'];
+% fullname = ['~/Dropbox/Uni/Exercises/12Semester/MAPhysics/data/' name '.h5'];
+fullname = ['~/Data/' name '.h5'];
 loadDsets;
 if ~exist('dsets', 'var')
     dsets = dsetsAll;
@@ -10,6 +11,6 @@ require('a', 't', dsets);
 % TODO decide whether I want certain indices for the datasets
 % create dsetsIndexed in loadDsets to indicate the ones I need to index
 
-for i = 1:length(dsets)
-    readDset(dictNames, fullname, dsets{i});
+for loopvar = 1:length(dsets)
+    readDset(dictNames, fullname, dsets{loopvar});
 end
