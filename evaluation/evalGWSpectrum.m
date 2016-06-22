67fa85a5f0ec62d62b1d1c8866a6adbf1c01aa03
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-21 20:55:05 -0700

    cosmetics

--- evaluation/evalGWSpectrum.m
+++ evaluation/evalGWSpectrum.m
@@ -8,13 +8,13 @@ nslices = 60;
 
 %% run
 % initialization
-require('gwps', 'rhoS', 'N', 'spatial_bounds_x');
+require('gwps','rhoS','N','spatial_bounds_x');
 readDsets;
 nbins = size(gwps,2);
 N = N(1);
-L = spatial_bounds_x(2) - spatial_bounds_x(1);
-kmin = 2*pi/L; kmax = sqrt(3) * N/2 * kmin;
-k = (1:nbins)/nbins * kmax;
+L = spatial_bounds_x(2)-spatial_bounds_x(1);
+kmin = 2*pi/L; kmax = sqrt(3)*kmin*N/2;
+k = (1:nbins)*kmax/nbins;
 % check for negative and nan values (just making sure)
 I = (gwps<0);
 if any(I(:))
@@ -35,9 +35,9 @@ range = 1:skip:length(a);
 for i = [range; 1:length(range)]
     subplot(1,2,1)
     loglog(k,gwps(i(1),:),'color',J(i(2),:));
-    xlabel('|k|'); ylabel('d\Omega_{gw}/d ln(k)'); hold on;
+    xlabel('|k|'); ylabel('d \Omega_{gw} / d ln(k)'); hold on;
     subplot(1,2,2)
     loglog(a(i(1)),rhorms(i(1)),'x','color',J(i(2),:));
-    xlabel('a'); ylabel('std(\rho)/<\rho>');
+    xlabel('a'); ylabel('std(\rho) / |<\rho>|');
 end
 hold off; shg;
