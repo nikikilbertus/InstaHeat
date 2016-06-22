f09c6c9b26541701fc1ea9d597e091e7ea8d3ca5
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-21 19:15:01 -0700

    cleanup

--- evaluation/evalMasses.m
+++ evaluation/evalMasses.m
@@ -1,28 +1,36 @@
 %% evaluate runs with different masses
 
 %% setup parameters
-% TODO
-ml = 3; m = 4;
-% masses = 1./(2*10.^(-(ml:m)));
-masses = 5*10.^(-(ml:m));
-rhormsi = zeros(m-ml+1,4);
-maxrhos = zeros(m-ml+1,1);
-for i = 1:m-ml+1
-    name = ['gw4/64_5e-' num2str(i+ml-1) '_2e4'];
-    evaluate3D
+% set up 'masses' array
+ml = 3; mh = 4;
+masses = 5*10.^(-(ml:mh));
+% construct the file names: prefix, suffix, indexset
+pre = 'gw4/64_5e-';
+suf = '_2e4';
+ind = ml:mh;
+
+%% run
+require('rhorms');
+nn = mh-ml+1;
+rhormsi = zeros(nn,4);
+maxrhos = zeros(nn,1);
+for i = 1:nn
+    name = [pre num2str(ind(i)) suf];
+    readDsets;
     maxrhos(i) = max(rhorms);
     loglog(a,rhorms/rhorms(1),'linewidth',2); hold on
     legendinfo{i} = ['mass=' num2str(masses(i))];
     rhormsi(i,1) = rhorms(1);
-    [~,idx] = min((a - 1e2).^2);
+    [~,idx] = min((a - a(end)/3).^2);
     rhormsi(i,2) = rhorms(idx);
-    [~,idx] = min((a - 1e3).^2);
+    [~,idx] = min((a - 2*a(end)/3).^2);
     rhormsi(i,3) = rhorms(idx);
     rhormsi(i,4) = rhorms(end);
 end
-hold off; xlabel('a'); ylabel('std(\rho) / |<\rho>|');
+hold off; xlabel('a'); ylabel('std(\rho)/|<\rho>|');
 legend(legendinfo,'location','southeast');
 figure
-loglog(masses, rhormsi, masses, masses/masses(1) * rhormsi(1,1) * 0.9,'--','linewidth',2);
-xlabel('mass'); ylabel('std(\rho) / |<\rho>|');
-legend('a=1', 'a=1e2', 'a=1e3', ['a=' num2str(a(end))],'linear reference','location','northwest');
+loglog(masses,rhormsi, masses,masses/masses(1)*rhormsi(1,1)*0.9,'--','linewidth',2);
+xlabel('mass'); ylabel('std(\rho)/|<\rho>|');
+legend('a=1', ['a=' num2str(a(end)/3)], ['a=' num2str(2*a(end)/3)], ...
+    ['a=' num2str(a(end))],'linear reference','location','northwest');
