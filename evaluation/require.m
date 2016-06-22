34af6886086a17d8c7ce1f7edb53d34f0e734f58
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-22 11:49:03 -0700

    require now has memory about dsets

--- evaluation/require.m
+++ evaluation/require.m
@@ -1,4 +1,8 @@
 function [] = require(varargin)
-    dsets = joinDsets(varargin{:});
+    if evalin('caller','exist(''dsets'',''var'')')
+        dsets = joinDsets(varargin{:}, evalin('caller','dsets'));
+    else
+        dsets = joinDsets(varargin{:});
+    end
     assignin('caller', 'dsets', dsets);
 end
