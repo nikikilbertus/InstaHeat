8b9dbf50891891de942ae6b6c1b9674327e97357
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-24 09:18:07 -0700

    added new output and plots

--- evaluation/evalMasses.m
+++ evaluation/evalMasses.m
@@ -18,9 +18,11 @@ indout = masses;
 nn = length(masses);
 name = [pre num2str(ind(1)) suf];
 readDsets;
-as = [1, max(a)/10, max(a)/2, max(a)];
+as = [1, max(a)/100, max(a)/10, max(a)];
 rhormsi = zeros(nn,4);
+rhoscali = zeros(nn,4);
 psistdi = zeros(nn,4);
+psimaxi = zeros(nn,4);
 for i = 1:nn
     name = [pre num2str(ind(i)) suf];
     if nout > 0
@@ -32,42 +34,58 @@ for i = 1:nn
     require('rhoS','psiS');
     readDsets;
     legendinfo{i} = ['mass=' num2str(masses(i))];
+    psiabsmax = max(abs(psimin),abs(psimax));
+    psiabsstd = abs(psistd);
+    rhoscal = max(abs(rhomin),abs(rhomax))./rhomean;
     figure(1);
-    subplot(1,2,1); loglog(a,rhorms); hold on;
-    subplot(1,2,2); loglog(a,rhorms/rhorms(1)); hold on;
+    subplot(1,3,1); loglog(a,rhorms); hold on;
+    subplot(1,3,2); loglog(a,rhorms/rhorms(1)); hold on;
+    subplot(1,3,3); loglog(a,rhoscal); hold on;
     figure(2);
-    subplot(1,2,1); loglog(a,abs(psistd)); hold on;
-    subplot(1,2,2); loglog(a,abs(psistd/psistd(1))); hold on;
-    rhormsi(i,1) = rhorms(1); % rhorms in the beginning
-    psistdi(i,1) = psistd(1); % psistd in the beginning
+    subplot(1,2,1); loglog(a,psiabsstd,a,psiabsmax); hold on;
+    subplot(1,2,2); loglog(a,psiabsstd/psiabsstd(1)); hold on;
+     % in the beginning
+    rhormsi(i,1) = rhorms(1);
+    rhoscali(i,1) = rhoscal(1);
+    psistdi(i,1) = psistd(1);
+    psimaxi(i,1) = psiabsmax(1);
+    % at first intermediate point of the evolution
     [~,idx] = min((a - as(2)).^2);
-    rhormsi(i,2) = rhorms(idx); % rhorms at one tenth of the evolution
-    psistdi(i,2) = psistd(idx); % psistd at one tenth of the evolution
-    [~,idx] = min((a - a(3)).^2);
-    rhormsi(i,3) = rhorms(idx); % rhorms at half of the evolution
-    psistdi(i,3) = psistd(idx); % psistd at half of the evolution
-    rhormsi(i,4) = rhorms(end); % rhorms in the end
-    psistdi(i,4) = psistd(end); % psistd in the end
+    rhormsi(i,2) = rhorms(idx);
+    rhoscali(i,2) = rhoscal(idx);
+    psistdi(i,2) = psistd(idx);
+    psimaxi(i,2) = psiabsmax(idx);
+    % at second intermediate point of the evolution
+    [~,idx] = min((a - as(3)).^2);
+    rhormsi(i,3) = rhorms(idx);
+    rhoscali(i,3) = rhoscal(idx);
+    psistdi(i,3) = psistd(idx);
+    psimaxi(i,3) = psiabsmax(idx); 
+    % at end
+    rhormsi(i,4) = rhorms(end);
+    rhoscali(i,4) = rhoscal(end);
+    psistdi(i,4) = psistd(end);
+    psimaxi(i,4) = psiabsmax(end);
     display(sprintf('processed %i of %i', i, nn));
     % output
     nameout = [preout num2str(indout(i)) sufout '.csv'];
-    psiabsmax = max(abs(psimin),abs(psimax));
-    psiabsstd = abs(psistd);
-    T = table(a,rhomean,rhorms,psiabsstd,psiabsmax);
+    T = table(a,rhomean,rhorms,rhoscal,psiabsstd,psiabsmax);
     writetable(T,nameout);
 end
 figure(1);
-subplot(1,2,1);
+subplot(1,3,1);
 for i = 1:4
-    loglog([as(i) as(i)], [1e-2 5e0],'black');
+    loglog([as(i) as(i)], [1e-3 5e0],'--black');
 end
 xlabel('a'); ylabel('std(\rho) / |<\rho>|'); legend(legendinfo,'location','southeast');
-subplot(1,2,2);
+subplot(1,3,2);
 xlabel('a'); ylabel('std(\rho) / |<\rho>| normalized'); legend(legendinfo,'location','southeast');
+subplot(1,3,3);
+xlabel('a'); ylabel('max(|\rho|)'); legend(legendinfo,'location','southeast');
 figure(2);
 subplot(1,2,1);
 for i = 1:4
-    loglog([as(i) as(i)], [1e-3 1e-1],'black');
+    loglog([as(i) as(i)], [1e-4 1e-2],'--black');
 end
 xlabel('a'); ylabel('std(\psi)'); legend(legendinfo,'location','southeast');
 subplot(1,2,2);
@@ -76,14 +94,15 @@ xlabel('a'); ylabel('std(\psi) normalized'); legend(legendinfo,'location','south
 figure
 loglog(masses,rhormsi, masses,masses/masses(1)*rhormsi(1,1)*0.9,'--','linewidth',2);
 xlabel('mass'); ylabel('std(\rho) / |<\rho>|');
-legend('a=1', ['a=' num2str(a(end)/3)], ['a=' num2str(2*a(end)/3)], ...
+legend('a=1', ['a=' num2str(as(2))], ['a=' num2str(as(3))], ...
     ['a=' num2str(a(end))],'linear reference','location','northwest');
 % various intermediate phistd values as function of masses
 figure
-loglog(masses,psistdi);
-xlabel('mass'); ylabel('std(\psi)');
-legend('a=1', ['a=' num2str(a(end)/3)], ['a=' num2str(2*a(end)/3)], ...
+loglog(masses,psimaxi);
+xlabel('mass'); ylabel('max(|\psi|)');
+legend('a=1', ['a=' num2str(as(2))], ['a=' num2str(as(3))], ...
     ['a=' num2str(a(end))],'location','northwest');
 return
 
-%% output
+T = table(masses',rhormsi,rhoscali,psistdi,psimaxi);
+writetable(T,'intermediate.csv');
