2bb805dd6b2002d1fe061293535bed2a0072fc42
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-27 16:34:04 -0700

    fixed and upgraded

--- evaluation/mk3dvideo.m
+++ evaluation/mk3dvideo.m
@@ -1,75 +1,85 @@
 %% create 3D volume visualization video
 
 %% setup (user input)
-name = 'cutoff/96_64_5e-3_fil';
+name = 'fullout/96_96_5e-3_1_reduced';
+fieldname = 'rho'; filename = 'rho3d';
 
 %% run
 require('a','rho','N');
 readDsets;
 field = rho;
-fieldname = 'rho'; filename = 'rho3d';
+clear rho
 Nt = length(a);
-field = reshape(field, N(1), N(2), N(3), Nt);
+showfrac = int64(0.1*N(1)*N(2)*N(3));
+field = reshape(field,Nt,N(1),N(2),N(3));
 
 x = linspace(0,1,N(1)+1)'; x = x(1:end-1);
 y = linspace(0,1,N(2)+1)'; y = y(1:end-1);
 z = linspace(0,1,N(3)+1)'; z = z(1:end-1);
 [X,Y,Z] = meshgrid(x,y,z);
 
-pl = field(:,:,:,end);
+pl = squeeze(field(end,:,:,:));
 [~,pos] = max(pl(:));
 [m1, m2, m3] = ind2sub(size(pl),pos);
 pad = 8;
-xx = x(m1-pad:m1+pad);
+xx = x(m1-pad-7:m1+pad-7);
 yy = y(m2-pad:m2+pad);
 zz = z(m3-pad:m3+pad);
 [XX,YY,ZZ] = meshgrid(xx,yy,zz);
 
+figure('Position',[1,1,2048,1152]);
 writerObj = VideoWriter([filename '.avi']);
 open(writerObj);
 set(gcf,'Renderer','zbuffer');
 % Create a sequence of frames and write each frame to the file
 for i = 1:length(a)
-pl = field(:,:,:,i);
-subplot(2,3,1)
-h = slice(X,Y,Z, pl, x, y, z, 'cubic');
-set(h, 'EdgeColor','none', 'FaceColor','interp')
-alpha(.1)
-subplot(2,3,2)
-pl1 = pl;
-plm = mean(pl1(:));
-pl1(abs(pl1 - plm)/abs(plm) < 0.95) = nan;
-hh = slice(X,Y,Z,pl1, x, y, z, 'cubic');
-set(hh, 'EdgeColor','none', 'FaceColor','interp')
-alpha(.3)
-title(['rho @ t = ' num2str(t(i)) ', a = ' num2str(a(i))]);
-subplot(2,3,3)
-pl2 = pl(m1-pad:m1+pad,m2-pad:m2+pad,m3-pad:m3+pad);
-% g = slice(pl, m1, m2, m3, 'cubic');
-g = slice(XX,YY,ZZ,pl2, x(m1), y(m2), z(m3), 'cubic');
-set(g, 'EdgeColor','none', 'FaceColor','interp')
-alpha(0.5)
-subplot(2,3,4)
-surf(y,z,log10(squeeze(pl(m1,:,:)))); xlabel('y'); ylabel('z');
-zlabel(['log10(' fieldname ')']);
-shading interp
-subplot(2,3,5)
-surf(x,z,log10(squeeze(pl(:,m2,:)))); xlabel('x'); ylabel('z');
-zlabel(['log10(' fieldname ')']);
-shading interp
-subplot(2,3,6)
-surf(x,y,log10(squeeze(pl(:,:,m3)))); xlabel('x'); ylabel('y');
-zlabel(['log10(' fieldname ')']);
-shading interp
+    pl = log10(squeeze(field(i,:,:,:)));
+    subplot(2,3,1)
+    h = slice(X,Y,Z,pl,x,y,z,'linear');
+    set(h,'EdgeColor','none','FaceColor','interp')
+    alpha(.1)
+    colormap jet
+    subplot(2,3,2)
+    pl1 = pl;
+    plm = mean(pl1(:));
+    pl1 = abs(pl1-plm)/abs(plm);
+    plsort = sort(pl1(:),'descend');
+    thresh = plsort(showfrac);
+    pl1(pl1 < thresh) = nan;
+    hh = slice(X,Y,Z,pl1,x,y,z,'linear');
+    set(hh,'EdgeColor','none','FaceColor','interp')
+    alpha(.3)
+    colormap jet
+    title(['rho @ a = ' num2str(a(i))]);
+    subplot(2,3,3)
+    pl2 = pl(m1-pad-7:m1+pad-7,m2-pad:m2+pad,m3-pad:m3+pad);
+    % g = slice(pl, m1, m2, m3, 'cubic');
+    g = slice(XX,YY,ZZ,pl2,x(m1),y(m2),z(m3),'linear');
+    set(g,'EdgeColor','none','FaceColor','interp')
+    alpha(0.5)
+    colormap jet
+    subplot(2,3,4)
+    surf(y,z,squeeze(pl(m1,:,:))); xlabel('y'); ylabel('z');
+    zlabel(['log10(' fieldname ')']);
+    shading interp
+    subplot(2,3,5)
+    surf(x,z,squeeze(pl(:,m2,:))); xlabel('x'); ylabel('z');
+    zlabel(['log10(' fieldname ')']);
+    shading interp
+    subplot(2,3,6)
+    surf(x,y,squeeze(pl(:,:,m3))); xlabel('x'); ylabel('y');
+    zlabel(['log10(' fieldname ')']);
+    shading interp
 
-frame = getframe(gcf);
-writeVideo(writerObj,frame);
+    frame = getframe(gcf);
+    writeVideo(writerObj,frame);
+    display(sprintf('processed %i of %i', i, length(a)));
 end
 close(writerObj);
 
-system(['ffmpeg -i rho3d.avi -vcodec libx264 -filter:v "setpts=15.0*PTS"'...
-    filename '.mp4'])
+system(['ffmpeg -i rho3d.avi -vcodec libx264 -filter:v "setpts=15.0*PTS" ' filename '.mp4'])
 
+return
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% initial conditions from karsten
 
