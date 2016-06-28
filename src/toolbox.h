5f0173e76f763e240d0d8890a47787dc33991932
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-06-27 18:22:20 -0700

    make fft,ifft public

--- src/toolbox.h
+++ src/toolbox.h
@@ -29,5 +29,7 @@ void mk_rhs(const double t, double *f, double *result);
 void prepare_and_save_timeslice();
 void mk_gradient_squared_and_laplacian(double *in);
 void mk_rho_and_p(const double *f);
+void fft(double *in, complex *out);
+void ifft(complex *in, double *out);
 
 #endif
