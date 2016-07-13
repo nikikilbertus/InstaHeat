bdd5e08aa80bdb08e93c681e38320d1a62410192
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-07-12 21:44:35 -0700

    enable various gsl integrators, format output

--- src/rkf45.c
+++ src/rkf45.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#if INTEGRATION_METHOD == RKF45
+#ifdef GSL_STEPPER
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,11 +27,19 @@ void run_rkf45()
     write_start_info();
     prepare_and_save_timeslice();
     gsl_odeiv2_system sys = {mk_rhs_wrap, NULL, pars.Ntot, NULL};
+    gsl_odeiv2_driver *d;
 
     //TODO: use gsl_odeiv2_driver_alloc_scaled_new for abs err vector
-    gsl_odeiv2_driver *d = gsl_odeiv2_driver_alloc_y_new(&sys,
-            gsl_odeiv2_step_rkf45, pars.t.dt,
+    #if INTEGRATION_METHOD == RKF45
+    d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45, pars.t.dt,
             ABSOLUTE_TOLERANCE, RELATIVE_TOLERANCE);
+    #elif INTEGRATION_METHOD == RKCK
+    d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkck, pars.t.dt,
+            ABSOLUTE_TOLERANCE, RELATIVE_TOLERANCE);
+    #elif INTEGRATION_METHOD == DOPRI89
+    d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk8pd, pars.t.dt,
+            ABSOLUTE_TOLERANCE, RELATIVE_TOLERANCE);
+    #endif
     gsl_odeiv2_driver_set_hmin(d, MINIMAL_DELTA_T);
 
     for (size_t i = 1; i <= RKF45_OUTPUT_NUMBER; ++i) {
@@ -70,13 +78,13 @@ static int mk_rhs_wrap(double t, const double f[], double res[], void *params)
 static void write_start_info()
 {
     INFO(puts("Starting rkf45 integration with:"));
-    INFO(printf("  Initial time: %.17f\n", pars.t.ti));
-    INFO(printf("  Final time: %.17f\n", pars.t.tf));
-    INFO(printf("  Initial time step dt: %.17f\n", pars.t.dt));
-    INFO(printf("  Minimal time step dt: %.17f\n", MINIMAL_DELTA_T));
-    INFO(printf("  Max number of steps: %f\n", MAX_STEPS));
+    INFO(printf("  Initial time: %g\n", pars.t.ti));
+    INFO(printf("  Final time: %g\n", pars.t.tf));
+    INFO(printf("  Initial time step dt: %g\n", pars.t.dt));
+    INFO(printf("  Minimal time step dt: %g\n", MINIMAL_DELTA_T));
+    INFO(printf("  Max number of steps: %g\n", MAX_STEPS));
     // TODO: change this once I have more definitions
-    INFO(printf("  Relative tolerance: %.17f\n", RELATIVE_TOLERANCE));
-    INFO(printf("  Absolute tolerance: %.17f\n\n", ABSOLUTE_TOLERANCE));
+    INFO(printf("  Relative tolerance: %g\n", RELATIVE_TOLERANCE));
+    INFO(printf("  Absolute tolerance: %g\n\n", ABSOLUTE_TOLERANCE));
 }
 #endif
