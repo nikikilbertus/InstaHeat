4411be779cda1724ccd8178bd5a6f54729830dbe
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-07-12 22:15:32 -0700

    generalize rkf45 to gsl_stepper

--- src/rkf45.h
+++ src/rkf45.h
@@ -1,15 +1,15 @@
-#ifndef __RKF45__
-#define __RKF45__
+#ifndef __GSL_STEPPER__
+#define __GSL_STEPPER__
 
 #include "main.h"
 #ifdef GSL_STEPPER
 
 /**
- * @file RKF45.h
- * @brief Function declarations for RKF45.c
+ * @file gsl_stepper.h
+ * @brief Function declarations for gsl_stepper.c
  */
 
-void run_rkf45();
+void run_gsl_stepper();
 
 #endif
 #endif
