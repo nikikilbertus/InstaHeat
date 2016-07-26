8b40ec31c8a3c77abad082fe506d683e028a933e
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-07-25 18:22:53 -0700

    find index of nearst value to given

---
+++ evaluation/getIndexClosestTo.m
@@ -0,0 +1,8 @@
+function [idx, warn] = getIndexClosestTo(f, val)
+    warn = 0;
+    [~, idx] = min(abs(f(:) - val));
+    if idx == 1 || idx == length(f)
+        disp 'Index at boundaries!'
+        warn = 1;
+    end
+end
