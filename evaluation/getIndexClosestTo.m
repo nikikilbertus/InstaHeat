dc89c9066b19f8b9287a35d0a5a7a60c8b585c9d
Niki Kilbertus <nikikilbertus@users.noreply.github.com>
2016-07-27 20:04:55 -0700

    minor fix

--- evaluation/getIndexClosestTo.m
+++ evaluation/getIndexClosestTo.m
@@ -1,7 +1,7 @@
 function [idx, warn] = getIndexClosestTo(f, val)
     warn = 0;
     [~, idx] = min(abs(f(:) - val));
-    if idx == 1 || idx == length(f)
+    if idx == 1 || idx == length(f(:))
         disp 'Index at boundaries!'
         warn = 1;
     end
